3b024ba44e53a0d340a33234e05a897d9f012248
Moin <76849575+Moinuddin9777@users.noreply.github.com>
2022-09-26 19:49:37 +0530

    Co-authored-by: Moin <76849575+Moinuddin9777@users.noreply.github.com>

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,7 +16,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Image.network(
+        'https://c.tenor.com/0AVbKGY_MxMAAAAM/check-mark-verified.gif',
+        height: 400,
+        width: 400,
+      )),
     );
   }
 }
